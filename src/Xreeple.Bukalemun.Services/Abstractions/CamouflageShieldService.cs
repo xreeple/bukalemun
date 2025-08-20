93923b04f3f15dce2ce13b6d1507860f73e2989c
Tuncay Cem Uzun <cem.uzun@d-teknoloji.com.tr>
2025-08-20 09:50:28 +0300

    Refactor CamouflageShield functionality and remove interface
    Removed the `CamouflageShieldService` interface and `CamouflageShieldMode` enum.
    The `CamouflageShieldService` class now directly implements masking functionality with a switch expression.
    Added private methods for various masking strategies while keeping the `CamouflageShieldOptions` class unchanged.

--- src/Xreeple.Bukalemun.Services/Abstractions/CamouflageShieldService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xreeple.Bukalemun.Services.Options;
-
-namespace Xreeple.Bukalemun.Services.Abstractions
-{
-    public interface CamouflageShieldService
-    {
-        string Mask(string input, CamouflageShieldOptions options);
-    }
-}
--- src/Xreeple.Bukalemun.Services/Options/CamouflageShieldOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Xreeple.Bukalemun.Services.Options
-{
-    public class CamouflageShieldOptions
-    {
-        public CamouflageShieldMode Mode { get; set; }
-        public int Count { get; set; } = 0;
-    }
-}
